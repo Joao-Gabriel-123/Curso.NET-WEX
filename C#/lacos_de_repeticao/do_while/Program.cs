20c5f5b501e89a93e003a3ccabefd08488eb8ba7
João <jgablopes2008@gmail.com>
2025-07-01 15:19:35 -0300

    Mais arquivos de C#

--- C#/lacos_de_repeticao/do_while/Program.cs
+++ C#/lacos_de_repeticao/do_while/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int soma = 0;
+int numero = 0;
+
+do
+{
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+} while (numero != 0);
+
+Console.WriteLine($"Total da soma dos números digitados é: {soma}");
--- C#/lacos_de_repeticao/while/Program.cs
+++ C#/lacos_de_repeticao/while/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numero = 5;
+int contador = 1;
+
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador}º execução: {numero} X {contador} = {numero * contador}");
+
+    // if (contador == 5)
+    // {
+    //     break;
+    // }
+
+    contador++;
+}
