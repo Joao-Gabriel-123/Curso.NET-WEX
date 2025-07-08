01c6d4d7179f3151e44b65bee523a14735d5ae2d
João <jgablopes2008@gmail.com>
2025-07-08 11:03:47 -0300

    Arquivos do SQL Server

---
+++ SQL Server/DDL/drop/drop.sql
@@ -0,0 +1,7 @@
+--Comando DROP, DELETA uma tabela ou banco de dados
+
+--Deletando um banco de dados, caso ele exista
+DROP DATABASE IF EXISTS Produtos 
+
+--Deletando uma tabela de produtos, caso ela exista
+DROP TABLE IF EXISTS dbo.Produtos
